521c9b99bfc5deb21053d52f3ad2fec4c9785dd6
malditoprotozoo <virodriguez.fe@gmail.com>
2017-10-03 21:31:08 -0300

    Caesar Cipher for my course at Laboratoria :3

---
+++ main.js
@@ -0,0 +1,54 @@
+//This loop will not rest until someone input a valid option 3:)
+
+function whatToCipher() {
+    do {
+        var answer = prompt("Si desea cifrar una frase, ingrese el número 1. Si desea descifrar, ingrese el número 2");
+        if (answer !== "") {
+            if (answer === "1") {
+                cipher();
+            } else if (answer === "2") {
+                decipher();
+            } else {
+                alert("Ingrese una opción válida ¬¬");
+            }
+        }
+    } while (answer === "" || (answer != "1" && answer != "2"));
+}
+
+
+//This is the function that will cipher any phrase. You can replace "33" with the number of words you want to shift
+
+
+function cipher(str) {
+    var str = prompt("¿Qué quieres cifrar?");
+    var output = "";
+    for (var i = 0; i < str.length; i++) {
+        var codeAscii = str.charCodeAt(i);
+        if (codeAscii >= 65 && codeAscii <= 90) {
+            output += String.fromCharCode((codeAscii - 65 + 33) % 26 + 65);
+        } else if (codeAscii >= 97 && codeAscii <= 122) {
+            output += String.fromCharCode((codeAscii - 97 + 33) % 26 + 97);
+        } else {
+            output += String.fromCharCode(codeAscii);
+        }
+    }
+    return document.write("Este es tu texto cifrado: \n" + output);
+}
+
+//This function will uncipher any ciphered phrase. 
+
+function decipher(str) {
+    var str = prompt("¿Qué quieres descifrar?");
+    var output = "";
+    for (var i = 0; i < str.length; i++) {
+        var codeAscii = str.charCodeAt(i);
+        if (codeAscii >= 65 && codeAscii <= 90) {
+            output += String.fromCharCode((codeAscii - 65 - 7 + 52) % 26 + 65);
+        } else if (codeAscii >= 97 && codeAscii <= 122) {
+            output += String.fromCharCode((codeAscii - 97 - 7 + 52) % 26 + 97);
+        } else {
+            output += String.fromCharCode(codeAscii);
+        }
+    }
+    return document.write("Este es tu texto descifrado: \n" + output);
+}
