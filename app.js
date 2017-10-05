5708574ba2547368132727c14f4c69c0c4854a98
Victoria Rodriguez <virodriguez.fe@gmail.com>
2017-10-05 09:46:58 -0300

    I fixed my code a little.

--- app.js
+++ app.js
@@ -16,15 +16,19 @@ function whatToCipher() {
 }
 
 
-//This is the function that will cipher any phrase. You can replace "33" with the number of words you want to shift
+/*This is the function that will cipher any phrase, but it'll ignore numbers.
+You can replace "33" with the number of words you want to shift */
 
 
 function cipher(str) {
-    str = prompt("¿Qué quieres cifrar?");
+    str = prompt("¿Qué quieres cifrar? Por favor, evita usar números.");
     var output = "";
     for (var i = 0; i < str.length; i++) {
         var codeAscii = str.charCodeAt(i);
-        if (codeAscii >= 65 && codeAscii <= 90) {
+        if (codeAscii >= 48 && codeAscii <= 57) {
+            alert("¡Los números no serán cifrados!");
+            break; //Use of break so the alert doesn't appear one time for each number.
+        } else if (codeAscii >= 65 && codeAscii <= 90) {
             output += String.fromCharCode((codeAscii - 65 + 33) % 26 + 65);
         } else if (codeAscii >= 97 && codeAscii <= 122) {
             output += String.fromCharCode((codeAscii - 97 + 33) % 26 + 97);
@@ -32,17 +36,20 @@ function cipher(str) {
             output += String.fromCharCode(codeAscii);
         }
     }
-    return prompt("Este es tu texto cifrado", output); //Prompt en vez de alert para que el usuario pueda copiar el texto.
+    return prompt("Este es tu texto cifrado", output); //Prompt instead of alert so the user can copy/paste the result.
 }
 
-//This function will uncipher any ciphered phrase. 
+//This function will uncipher any ciphered phrase, but it'll ignore the numbers. 
 
 function decipher(str) {
     str = prompt("¿Qué quieres descifrar?");
     var output = "";
     for (var i = 0; i < str.length; i++) {
         var codeAscii = str.charCodeAt(i);
-        if (codeAscii >= 65 && codeAscii <= 90) {
+        if (codeAscii >= 48 && codeAscii <= 57) {
+            alert("¡Los números no serán descifrados!");
+            break;
+        } else if (codeAscii >= 65 && codeAscii <= 90) {
             output += String.fromCharCode((codeAscii - 65 - 7 + 52) % 26 + 65);
         } else if (codeAscii >= 97 && codeAscii <= 122) {
             output += String.fromCharCode((codeAscii - 97 - 7 + 52) % 26 + 97);
